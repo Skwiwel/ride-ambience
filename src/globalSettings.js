c6a2f3377852d1753ed88ee348740efadfb04a52
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2020-07-08 19:22:19 +0200

    fullscreen button

--- src/globalSettings.js
+++ src/globalSettings.js
@@ -1,10 +1,43 @@
 var globalSettings = new (function () {
   this.videoControls = new EmittingVariable(false);
+  this.fullscreen = new EmittingVariable(false);
   var _this = this;
 
   this.videoControls.addListener(function () {
     setCookie("videoControls", _this.videoControls.get());
   });
+
+  this.fullscreen.addListener(function () {
+    const doc = document.documentElement;
+    if (_this.fullscreen.get() === true) {
+      if (doc.requestFullscreen) {
+        doc.requestFullscreen();
+      } else if (doc.mozRequestFullScreen) {
+        /* Firefox */
+        doc.mozRequestFullScreen();
+      } else if (doc.webkitRequestFullscreen) {
+        /* Chrome, Safari and Opera */
+        doc.webkitRequestFullscreen();
+      } else if (doc.msRequestFullscreen) {
+        /* IE/Edge */
+        doc.msRequestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        /* Firefox */
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        /* IE/Edge */
+        document.msExitFullscreen();
+      }
+    }
+  });
+
   // init
   {
     /* Enable video controls if they were enabled last time */
--- src/mainMenu.js
+++ src/mainMenu.js
@@ -1,8 +1,19 @@
 var mainMenu = new (function () {
   const container = document.getElementById("main-menu-container");
+  const buttonFullscreen = document.getElementById("fullscreen-enable-button");
   const buttonVideoControls = document.getElementById(
     "video-controls-enable-button"
   );
+
+  buttonFullscreen.onclick = function () {
+    globalSettings.fullscreen.set(!globalSettings.fullscreen.get());
+    console.log(globalSettings.fullscreen.get());
+  };
+  updateFullscreenButton = function () {
+    buttonFullscreen.dataset.state = globalSettings.fullscreen.get();
+  };
+  globalSettings.fullscreen.addListener(updateFullscreenButton);
+
   buttonVideoControls.onclick = function () {
     globalSettings.videoControls.set(!globalSettings.videoControls.get());
   };
@@ -10,8 +21,10 @@ var mainMenu = new (function () {
     buttonVideoControls.dataset.state = globalSettings.videoControls.get();
   };
   globalSettings.videoControls.addListener(updateVideoControlsButton);
+
   // init
   {
+    updateFullscreenButton();
     updateVideoControlsButton();
   }
 })();
