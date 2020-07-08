6ddf6ccf5a685171dd1d2d3e3cf2efa475d1f31b
Kuba Grzechociński <kuba.grzechocinski@gmail.com>
2020-07-08 16:09:10 +0200

    added EmittingObject class

---
+++ src/EmittingObject.js
@@ -0,0 +1,21 @@
+function EmittingObject(val = undefined) {
+  var value = val;
+  var listeners = [];
+  this.set = function (val) {
+    value = val;
+    for (const i in listeners) listeners[i]();
+  };
+  this.setSilent = function (val) {
+    value = val;
+  };
+  this.get = function () {
+    return value;
+  };
+  this.addListener = function (fun) {
+    if (fun && typeof fun == "function") listeners.push(fun);
+  };
+  this.removeListener = function (fun) {
+    const index = listeners.indexOf(fun);
+    if (index > -1) listeners.splice(index, 1);
+  };
+}
